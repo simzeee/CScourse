8c305dd6238827c4a438051c54256d144ba4848f
simzeee <jwsims816@gmail.com>
2021-08-15 20:16:35 -0500

    insertion sort

--- specs/insertion-sort/insertion-sort.test.js
+++ specs/insertion-sort/insertion-sort.test.js
@@ -32,7 +32,7 @@ insertionSort([3,4,2,1])
 
 // unit tests
 // do not modify the below code
-test.skip("insertion sort", function () {
+test("insertion sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   insertionSort(nums);
   expect(nums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
